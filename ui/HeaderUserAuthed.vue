0a39050409b15c18ba1a07384e444c44935e4fd7
Tatianka-hk <t0681689150@gmail.com>
2026-02-03 22:32:44 +0100

    change logic of adding cards and change logic of learn

---
+++ ui/HeaderUserAuthed.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="w-full justify-between flex mb-[20px]">
+        <VButton
+            @click="$router.back()"
+            class="w-[42px] h-[42px] flex items-center justify-center"
+        >
+            <IconBack class="w-[24px] h-[24px] min-w-[24px]" />
+        </VButton>
+        <VButton
+            @click="logoutHandle()"
+            class="w-[42px] h-[42px] flex items-center justify-center"
+        >
+            <IconLogout class="w-[24px] h-[24px] min-w-[24px]" />
+        </VButton>
+    </div>
+</template>
+<script setup lang="ts">
+import { IconLogout, IconBack } from '~/assets/icons/'
+import { logout } from '~/apis/auth'
+
+const logoutHandle = () => {
+    try {
+        logout()
+    } catch (err) {
+        console.log(err)
+    }
+}
+</script>
